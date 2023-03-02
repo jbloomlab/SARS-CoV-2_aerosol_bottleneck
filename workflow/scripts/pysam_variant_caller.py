81e9aa1396d619ae95082ad44beb20149d583aed
Will Hannon <wwh22@uw.edu>
2023-03-01 19:58:28 -0800

    Add script to parse VCF files and call variants

---
+++ workflow/scripts/pysam_variant_caller.py
@@ -0,0 +1,375 @@
+"""
+The purpose of this script is to call variants from a BAM file using pysam.
+
+Author: Will Hannon
+"""
+from collections import defaultdict
+import argparse
+import pysam  
+import pandas as pd 
+from Bio import SeqIO
+
+
+def check_read(read):
+    """
+    Helper function to decide what reads should
+    be keep when parsing alignment file.
+
+    Parameters
+    ----------
+    read : pysam.AlignedSegment
+        read from an aligned BAM file
+
+    Returns
+    -------
+    bool
+        True/False if read should be included
+        
+    """
+    # Exclude Quality Failures
+    if read.is_qcfail:
+        return False
+    # Exclude Secondary Mappings
+    elif read.is_secondary:
+        return False
+    # Exclude Unmapped Reads
+    elif read.is_unmapped:
+        return False
+    # Exclude Marked Duplicates
+    elif read.is_duplicate:
+        return False
+    else:
+        return True
+        
+
+def call_variants(bampath, reference, contig, max_Depth = 100000, min_BQ = 25, min_Freq = 0.005, min_Cov = 1, callback = check_read):
+    """
+    Call variants from a pileup file using pysam.
+    
+    
+    Parameters
+    ----------
+    bampath : str
+        Path to an aligned, sorted, and indexed BAM file
+    reference : str
+        The reference sequence from a parsed FASTA file
+    contig : str
+        The chromosome name from a parsed FASTA file
+    min_BQ : int
+        The minimum base quality for a read to be considered at a position
+    min_Cov : int
+        The minimum depth for a position to be considered
+    callback : function
+        A function that return True if a read should be considered
+    
+
+    Returns
+    -------
+    pd.DataFrame
+        A table for variants identified in a given sample
+        
+        
+    """
+    # Store the rows
+    rows = []
+
+    # Read in the indexed BAM file
+    with pysam.AlignmentFile(bampath, "rb") as bamfile:
+        # Iterate over each column (a.k.a. position) in the pileup
+        for pileupcolumn in bamfile.pileup(contig, stepper = 'nofilter', min_base_quality = min_BQ, max_depth = max_Depth):
+
+            # Site-specific information
+            POS = pileupcolumn.reference_pos + 1
+            REF = reference[pileupcolumn.reference_pos]
+            DP = 0
+            RDF = 0
+            RDR = 0
+            RBQ = []
+            ALT = {base:{'ADF': 0,
+                         'ADR': 0,
+                         'ABQ': []
+                        } for base in 'ATCG' if base != REF}
+            
+            # Log a progress message
+            if POS % 1000 == 0:
+                print(f"Called variants for {POS} of {len(reference)} total bases...")
+
+            # Iterate over each read in the pileup
+            for pileupread in pileupcolumn.pileups:
+                # Skip reads that fail the QC requirements or contain indels
+                if pileupread.is_refskip or pileupread.is_del or not callback(pileupread.alignment):
+                    continue
+
+                # Increment Total Depth 
+                DP += 1
+
+                # Collect Read-specific information 
+                readpos = pileupread.query_position
+                base = pileupread.alignment.query_sequence[readpos]
+                qual = pileupread.alignment.query_qualities[readpos]
+                orientation = 'reverse' if pileupread.alignment.is_reverse else 'forward'
+
+                # Check if the site is the reference base
+                if base == REF:
+                    RBQ.append(qual)
+                    if orientation == "forward":
+                        RDF += 1
+                    else:
+                        RDR += 1
+                # Or, fill in SNPs at this position 
+                elif base != REF: 
+                    ALT[base]['ABQ'].append(qual)
+                    if orientation == "forward":
+                        ALT[base]['ADF'] += 1
+                    else:
+                        ALT[base]['ADR'] += 1
+
+            # Collect all of the information for this positions
+            for SNP, SNP_stats in ALT.items():
+                if SNP_stats['ADF'] + SNP_stats['ADR'] > 0:
+                    AF = (SNP_stats['ADF'] + SNP_stats['ADR']) / DP
+                    row = [POS, REF, SNP, round(AF, 4), DP, RDF, RDR, SNP_stats['ADF'], SNP_stats['ADR'], round(sum(RBQ)/len(RBQ), 2), round(sum(SNP_stats['ABQ'])/len(SNP_stats['ABQ']), 2)]
+                    rows.append(row)
+
+    # Convert the rows into a dataframe
+    variant_df = pd.DataFrame(rows, columns=["POS", "REF", "ALT", "AF", "DP", "RDF", "RDR", "ADF", "ADR", "RBQ", "ABQ"])
+    variant_df['CALLER'] = 'pysam'    
+    
+    # Apply any remaining filters
+    variant_df = variant_df.loc[variant_df["AF"] >= min_Freq]
+    variant_df = variant_df.loc[variant_df["DP"] >= min_Cov]
+    
+    return variant_df.reset_index(drop=True)
+
+
+def translate(codon):
+    """
+    Translate a three letter DNA string into 
+    a one letter amino acid code. 
+
+    Parameters
+    ----------
+    codon : str
+        three letter DNA sequence
+
+    Returns
+    -------
+    str
+        one letter amino acid code
+
+    Raises
+    ------
+    AssertionError
+        error if codon sequence is invalid
+        
+    """
+    
+    table = { 
+        'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M', 
+        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T', 
+        'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K', 
+        'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',                  
+        'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L', 
+        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P', 
+        'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q', 
+        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R', 
+        'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V', 
+        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A', 
+        'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E', 
+        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G', 
+        'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S', 
+        'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L', 
+        'TAC':'Y', 'TAT':'Y', 'TAA':'*', 'TAG':'*', 
+        'TGC':'C', 'TGT':'C', 'TGA':'*', 'TGG':'W', 
+    } 
+    
+    assert codon in table.keys(), "Not a valid codon sequence."
+    
+    return table[codon]
+
+
+def parse_gff_file(filename):
+    """
+    Generic parsing function for GFF3 format files.
+    
+    Parameters
+    ----------
+    filename : str
+        Path to GFF3 format annotation file
+
+    Returns
+    -------
+    dict
+        Dictionary of parsed features and header
+        
+    """
+    
+    data = {
+        'header': {},
+        'features': []
+    }
+    
+    with open(filename) as f:
+        print(f"Parsing {filename}...\n")
+        
+        for line in f:
+            line = line.strip()
+            
+            if line.startswith('###'):
+                print("End of file.")
+                break
+            elif line.startswith('##'):
+                key, value = line[2:].split(' ', 1)
+                data['header'][key] = value
+            elif line.startswith('#'):
+                continue
+            else:
+                fields = line.split('\t')
+                feature = {
+                    'contig': fields[0],
+                    'source': fields[1],
+                    'type': fields[2],
+                    'start': int(fields[3]),
+                    'end': int(fields[4]),
+                    'score': fields[5],
+                    'strand': fields[6],
+                    'phase': fields[7],
+                    'attributes': {}
+                }
+                
+                # Parse the attributes field
+                for attribute in fields[8].split(';'):
+                    if not attribute:
+                        continue
+                    key, value = attribute.split('=')
+                    feature['attributes'][key] = value
+                
+                data['features'].append(feature)
+                
+    return data
+
+
+def annotate_coding_change(position, alt, gff, reference):
+    """
+    Given a mutation and alternative base, annotate the coding 
+    effect if there is one.
+    
+    Parameters
+    ----------
+    position : int
+        1-indexed integer position of mutation in reference genome
+        
+    alt : str
+        Alternative base at this position
+    
+    gff : dict
+        Parsed dictionary of GFF annotations 
+        
+    reference : str
+        Reference sequence from fasta file
+        
+        
+    Returns
+    -------
+    list
+        List of [REF_AA, PROT_POS, ALT_AA, GENE]
+    
+    """
+    
+    # Extract the coding regions from the GFF file
+    CDS = defaultdict(list)
+    for feature in gff['features']:
+        if feature['type'] != 'CDS':
+            continue
+        # Append the gene name, start and stop (1-indexed)
+        gene = feature['attributes']['gene']
+        start = feature['start']
+        stop = feature['end']
+        CDS[gene].append([start, stop])
+        
+    # Annotate any possible coding changes
+    coding_changes = []
+    for gene, regions in CDS.items():
+        # Note that one mutation can have multiple coding effects in a gene (overlapping RFs and frameshifts)
+        for [start, stop] in regions:
+            if position >= start and position <= stop:
+                # Position of the SNP in the gene (0-index)
+                gene_pos = position - start
+                # Position of the SNP in the codon (0-index)
+                codon_pos = gene_pos % 3
+                # Position of the ALT_AA in the protein (0-index)
+                prot_pos = (gene_pos // 3) 
+                # Gene sequence 
+                gene_seq = str(reference)[start-1:stop]
+                # List of the codons in the gene
+                codons = [gene_seq[i:i+3] for i in range(0, len(gene_seq), 3)]
+                # Reference Codon
+                ref_codon = codons[prot_pos]
+                # Alternative Codon
+                alt_codon = "".join(alt if (i == codon_pos) else base for i, base in enumerate(ref_codon))
+                # Reference AA
+                ref_aa = translate(ref_codon)
+                # Alternative AA
+                alt_aa = translate(alt_codon)
+                
+                # Append the coding changes
+                coding_changes.append([ref_aa, prot_pos + 1, alt_aa, gene])
+
+    # Join multiple annotations if they exist       
+    if coding_changes:
+        return [":".join(str(x) for x in y) if len(set(y)) > 1 else y[0] for y in zip(*coding_changes)]
+    else:
+        return [None, None, None, None]
+
+
+def main():
+
+    # Initialize the argument parser
+    parser = argparse.ArgumentParser(
+        description='Call variants from a BAM file using Pysam.')
+    parser.add_argument('-b', '--bam', type=str, required=True,
+                        help='Path to input BAM file sorted and indexed')
+    parser.add_argument('-r', '--reference', type=str, required=True,
+                        help='Path to a reference genome in FASTA format')
+    parser.add_argument('-g', '--gff', type=str, required=True,
+                        help='Path to annotation file in GFF3 format')
+    parser.add_argument('-o', '--output', type=str, required=True,
+                        help='Path to output TSV file')
+    parser.add_argument('-q', '--min-baseQ', type=int, default=25,
+                        help='Minimum base quality to consider a read')
+    parser.add_argument('-d', '--max-depth', type=int, default=100000,
+                        help='Maximum depth of reads per position')
+    parser.add_argument('-f', '--min-freq', type=float, default=0.005,
+                        help='Minimum frequency to call a report a variant')
+    parser.add_argument('-c', '--min-coverage', type=int, default=1,
+                        help='Minimum coverage to include a position')
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Parse the GFF file
+    gff = parse_gff_file(args.gff)
+
+    # Parse the reference genome
+    refrecord = SeqIO.read(args.reference, "fasta")
+    contig = refrecord.name
+    reference = refrecord.seq
+
+    # Call variants 
+    variant_df = call_variants(args.bam,
+                               reference,
+                               contig,
+                               min_BQ = args.min_baseQ,
+                               min_Freq = args.min_freq,
+                               min_Cov = args.min_coverage,
+                               max_Depth = args.max_depth,
+                               callback = check_read)
+    
+    # Annotate the coding changes
+    variant_df[['REF_AA', 'POS_AA', 'ALT_AA', 'GENE']] = variant_df.apply(lambda row: pd.Series(annotate_coding_change(row['POS'], row['ALT'], gff, reference)), axis=1)
+
+    # Write the output to a TSV file
+    variant_df.to_csv(args.output, sep='\t', index=False)
+
+
+if __name__ == '__main__':
+    main()
---
+++ workflow/scripts/vcf_to_tsv.py
@@ -0,0 +1,159 @@
+"""
+The purpose of this script is to convert a VCF file to a TSV file.
+
+Author: Will Hannon
+"""
+import re
+import argparse
+import vcf
+import pandas as pd
+
+
+def one_letter_format(aa):
+    """
+    Convert 3 letter amino acid format to 1 letter amino acid format
+
+    Parameters
+    ----------
+    aa: str
+        Amino acid in three letter format
+
+    Returns
+    -------
+    str
+        Amino acid in one letter format
+
+    """
+    amino_acids = {
+        'Ala': 'A', 'Arg': 'R', 'Asn': 'N', 'Asp': 'D',
+        'Asx': 'B', 'Cys': 'C', 'Glu': 'E', 'Gln': 'Q',
+        'Glx': 'Z', 'Gly': 'G', 'His': 'H', 'Ile': 'I',
+        'Leu': 'L', 'Lys': 'K', 'Met': 'M', 'Phe': 'F',
+        'Pro': 'P', 'Ser': 'S', 'Thr': 'T', 'Trp': 'W',
+        'Tyr': 'Y', 'Val': 'V', '*': '*', 'Ter': '*'
+    }
+    return amino_acids[aa]
+
+
+def vcf_to_table(vcf_path, caller, annotations=False):
+    """
+    Read a vcf file created with either lofreq or varscan
+    into a pandas dataframe.
+
+    Parameters
+    ----------
+    vcf_path : str
+        Path to a VCFv4.0/VCFv4.1 format file from lofreq or varscan
+    caller : ['lofreq', 'varscan']
+        The variant calling program was used to generate the vcf file
+    annotations : bool
+        Shoud annotations made with SnpEFF be parsed from the file
+
+    Returns
+    -------
+    pd.DataFrame
+        A pandas dataframe containing the key fields
+
+    """
+
+    # Make sure that caller is a viable option
+    if caller not in {'lofreq', 'varscan'}:
+        raise ValueError(
+            f"{caller} is not one of the supported options: ['lofreq', 'varscan']")
+
+    # Make an empty list to store vcf information
+    vcf_info = []
+
+    # Read in the vcf file from provided path
+    with open(vcf_path, 'r') as vcf_file:
+        vcf_reader = vcf.Reader(vcf_file)
+
+        # Iterate over each record in the file
+        for record in vcf_reader:
+
+            # Get the fields common to all formats
+            POS = record.POS
+            REF = record.REF
+            ALT = record.ALT[0]
+
+            # Get infomration from lofreq formatted file
+            if caller == "lofreq":
+                DP = record.INFO['DP']
+                AF = record.INFO['AF']
+                RDF, RDR, ADF, ADR = record.INFO['DP4']
+                RBQ = None
+                ABQ = None
+
+            # Get the information from a varscan formatted file
+            if caller == "varscan":
+                # There will be only a single sample per record
+                for sample in record.samples:
+                    DP = sample['DP']
+                    AF = sample['FREQ'].strip('%') / 100
+                    RDF = sample['RDF']
+                    RDR = sample['RDR']
+                    ADF = sample['ADF']
+                    ADR = sample['ADR']
+                    RBQ = sample['RBQ']
+                    ABQ = sample['ABQ']
+
+            # Get the annotations if they're present
+            if annotations:
+                ANN = record.INFO['ANN'][0].split("|")
+                GENE = ANN[3]
+                match = re.match(
+                    r'^p\.([a-zA-Z]{3})(\d+)([a-zA-Z]{3})$', ANN[10])
+                if match:
+                    REF_AA = one_letter_format(match.group(1))
+                    POS_AA = match.group(2)
+                    ALT_AA = one_letter_format(match.group(3))
+                else:
+                    REF_AA = None
+                    POS_AA = None
+                    ALT_AA = None
+                # Add the fields from this record
+                vcf_info.append([POS, REF, ALT, AF, DP, RDF, RDR, ADF,
+                                ADR, RBQ, ABQ, GENE, REF_AA, POS_AA, ALT_AA])
+            # If there aren't annotations, return the full
+            else:
+                # Add the fields from this record
+                vcf_info.append(
+                    [POS, REF, ALT, AF, DP, RDF, RDR, ADF, ADR, RBQ, ABQ])
+
+    # Write the info out to a dataframe
+    if annotations:
+        vcf_df = pd.DataFrame(vcf_info,
+                              columns=["POS", "REF", "ALT", "AF", "DP", "RDF", "RDR", "ADF", "ADR", "RBQ", "ABQ", "GENE", "REF_AA", "POS_AA", "ALT_AA"])
+        vcf_df['CALLER'] = caller
+    else:
+        vcf_df = pd.DataFrame(vcf_info,
+                              columns=["POS", "REF", "ALT", "AF", "DP", "RDF", "RDR", "ADF", "ADR", "RBQ", "ABQ"])
+        vcf_df['CALLER'] = caller
+
+    return vcf_df
+
+
+def main():
+    # Initialize the argument parser
+    parser = argparse.ArgumentParser(
+        description='Convert a lofreq or varscan fromat VCF file into a TSV file.')
+    parser.add_argument('-i', '--input', type=str, required=True,
+                        help='Path to input VCF file in lofreq or varscan format')
+    parser.add_argument('-c', '--caller', type=str, required=True,
+                        help='Which variant caller was used to generate the VCF file: (lofreq or varscan))')
+    parser.add_argument('-a', '--add_annotations', action='store_true',
+                        help='Add annotations to output file')
+    parser.add_argument('-o', '--output', type=str, required=True,
+                        help='Path to output TSV file')
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Convert the vcf file to a tsv file
+    vcf_df = vcf_to_table(args.input, args.caller, args.add_annotations)
+
+    # Write the dataframe to a tsv file
+    vcf_df.to_csv(args.output, sep='\t', index=False)
+
+
+if __name__ == "__main__":
+    main()
